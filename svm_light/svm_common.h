81bb7b865d618887483892a88becdfc6f57d03a8
kr4uzi <kr4uzi@gmail.com>
2016-01-19 09:39:23 +0100

    FVAL using float again (double actually never needed)

--- svm_light/svm_common.h
+++ svm_light/svm_common.h
@@ -34,7 +34,7 @@
                              /* kernel evaluations. Using float saves */
                              /* us some memory, but you can use double, too */
 # define FNUM    long        /* the type used for storing feature ids */
-# define FVAL    double      /* the type used for storing feature values */
+# define FVAL    float       /* the type used for storing feature values */
 # define MAXFEATNUM 99999999 /* maximum feature number (must be in
 			  	valid range of FNUM type and long int!) */
 
